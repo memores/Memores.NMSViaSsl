5f1ff99f4521fbf7bffeadf6f20fbfdccde1a456
Павел <pdorovsky@gmail.com>
2018-12-11 15:41:09 +0300

    SslTcpTransportFactory with TcpTransport via SslStream

---
+++ Memores.NMSViaSsl/SslTcpTransport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS.ActiveMQ.Transport;
+using Apache.NMS.ActiveMQ.Transport.Tcp;
+
+namespace Memores.NMSViaSsl {
+    public class SslTcpTransport : TcpTransport {
+        public SslTcpTransport(Uri uri, Socket socket, IWireFormat wireformat) : base(uri, socket, wireformat) { }
+
+        protected override Stream CreateSocketStream() {
+            return new SslStream(new NetworkStream(socket));
+        }
+    }
+}
---
+++ Memores.NMSViaSsl/SslTcpTransportFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Transport;
+using Apache.NMS.ActiveMQ.Transport.Tcp;
+
+namespace Memores.NMSViaSsl {
+    [ActiveMQTransportFactory("ssl")]
+    public class SslTcpTransportFactory : TcpTransportFactory {
+        protected override ITransport DoCreateTransport(Uri location, Socket socket, IWireFormat wireFormat) {
+            return new SslTcpTransport(location, socket, wireFormat) {
+                SendBufferSize = SendBufferSize,
+                ReceiveBufferSize = ReceiveBufferSize
+            };
+        }
+    }
+}
